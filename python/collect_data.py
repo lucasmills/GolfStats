0a4dffab8dd0f705769f8acfc981750f9e75ac87
lucas mills <lucascmills1@gmail.com>
2024-12-28 14:24:57 -0500

    Add scores from 28 DEC 2024

--- python/collect_data.py
+++ python/collect_data.py
@@ -13,7 +13,8 @@ course_handicaps = read_golf_data(current_wd, PATH_TO_DATA, HANDICAPS)
 course_par_data = read_golf_data(current_wd, PATH_TO_DATA, COURSE_PAR)
 scorecard_history = read_golf_data(current_wd, PATH_TO_DATA, SCORE_CARDS)
 
-scores_and_points = collect_score_and_calculate_points(scorecard_history, course_par_data)
+scores_and_points = collect_score_and_calculate_points(scorecard_history,
+                                                       course_par_data)
 
 # Pickle the data
 db_file = open("data\\golf_data.pkl", 'ab')
@@ -21,7 +22,6 @@ db_file = open("data\\golf_data.pkl", 'ab')
 # source, destination
 pickle.dump(scores_and_points, db_file)
 db_file.close()
-
 
 # plot_golf_statistics(scores_and_points)
 
