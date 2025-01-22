b3dc3d4708b0194d337dcee87c8ae85e3076f4a4
lucas mills <lucascmills1@gmail.com>
2025-01-21 22:05:51 -0500

    Make web load a try except

--- utils/data_load.py
+++ utils/data_load.py
@@ -3,12 +3,14 @@
 import pandas
 
 
-def load_data_util(path, web=True):
-    if web:
-        path = "/home/millsgolfstats/GolfStats/" + path
-    # Load golf data
-    with open(path, "rb") as f:
-        golf_data = pandas.read_pickle(f)
+def load_data_util(path):
+    try:
+        with open(path, "rb") as f:
+            golf_data = pandas.read_pickle(f)
+    except FileNotFoundError:
+        web_path = "/home/millsgolfstats/GolfStats/" + path
+        with open(web_path, "rb") as f:
+            golf_data = pandas.read_pickle(f)
 
     return golf_data
 
