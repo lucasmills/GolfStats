7db913017c2d379fdb2570365892a571cfaff4c8
lucas mills <lucascmills1@gmail.com>
2025-01-26 15:23:35 -0500

    Add custom margins and legends

--- layout/figures/score_type_histogram.py
+++ layout/figures/score_type_histogram.py
@@ -5,7 +5,7 @@ import plotly.graph_objects as go
 
 
 # Generator
-def generate_score_type_histogram(golf_data):
+def generate_score_type_histogram(golf_data, margins):
     fig = go.Figure()
     fig.add_trace(go.Histogram(x=golf_data["Birdies"], name="Birdies"))
     fig.add_trace(go.Histogram(x=golf_data["Pars"], name="Pars"))
@@ -18,5 +18,18 @@ def generate_score_type_histogram(golf_data):
     fig.update_traces(opacity=0.75)
 
     fig.update_layout(xaxis_title="Average per round", yaxis_title="Frequency")
+
+    fig.update_layout(
+        autosize=True,
+        margin=margins
+    )
+
+    fig.update_layout(legend=dict(
+        orientation="h",
+        yanchor="bottom",
+        y=1.02,
+        xanchor="center",
+        x=1
+    ))
 
     return fig
